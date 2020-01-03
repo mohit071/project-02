d2212b6bbda10abb959827319fbbeba3dedf8d43
mohit071 <52122074+mohit071@users.noreply.github.com>
2020-01-03 16:36:12 +0530

    Update HomeServlet.java

--- src/main/java/com/javamaster/controller/HomeServlet.java
+++ src/main/java/com/javamaster/controller/HomeServlet.java
@@ -37,7 +37,7 @@ public class HomeServlet extends HttpServlet {
 		String path = request.getServletPath();
 		if (path.equals("/")){
 			List<Article> articles = articleDao.getAllArticles();
-			request.setAttribute("title", "Branch One");
+			request.setAttribute("title", "Staging");
 			request.setAttribute("articles", articles);
 			request.getRequestDispatcher("/WEB-INF/view/index.jsp").forward(request, response);
 		}
